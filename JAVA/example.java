355b6dddd49217cab74592e8a7bccddae8edcf38
G SHOBANA <shobikrishnan2005@gmail.com>
2024-08-24 12:01:15 +0530

    Delete JAVA directory

--- JAVA/example.java
+++
@@ -1,9 +0,0 @@
-class example {
-    public static void main(String[] args) 
-{
-        System.out.println("First Line");
-        System.out.print("Second Line");
-        System.out.println("Third Line");
-        System.out.print("Fourth Line");
-    }
-}
